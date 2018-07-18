6fda551834c554e40eb5bd425770e51a56dd6db0
Artemish <mitch.cro@gmail.com>
2018-07-18 14:46:10 -0500

    Restructured crawling method

--- salescraper/spiders/bar_spider.py
+++ salescraper/spiders/bar_spider.py
@@ -10,12 +10,14 @@ class BarsSpider(scrapy.Spider):
 
     def parse(self, response):
         for a in response.css('div.ttl').css('a'):
-            yield {
-                    'title': a.attrib['title'],
-                    'link': a.attrib['href']
-            }
+            item_page = response.urljoin(a.attrib['href'])
+            yield scrapy.Request(item_page, callback=self.parse_item_page)
 
         next_page = response.css('td.next a.enabled::attr(href)').extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse_item_page(self, response):
+        title = response.css('h1.it-ttl::text').extract_first()
+        yield {'title': title}#, 'response': response}
